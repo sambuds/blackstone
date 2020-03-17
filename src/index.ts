c6de260b778bbfd74f70991c7cf289ec667a953a
Gregory Hill <gregorydhill@outlook.com>
2020-03-17 13:44:39 +0000

    expose callTx
    Signed-off-by: Gregory Hill <gregorydhill@outlook.com>

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ export { Contracts, NewContracts } from './lib/contracts'
 export { Synched, NewSynched, VentListener } from './lib/vent';
 export { Deploy } from './deploy';
 export { BytesFromString, BytesToString } from './lib/utils';
+export { CallTx } from '@hyperledger/burrow/proto/payload_pb';
